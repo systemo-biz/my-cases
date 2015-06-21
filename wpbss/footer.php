da9e8a5f1b95b4a2d6064519c04013f24cf8989a
Asche98 <super-elena11@mail.ru>
2015-06-21 13:45:31 +0500

    load components

---
+++ wpbss/footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package wpbss
+ */
+?>
+        </div><!-- .row -->
+	</div><!-- #content .container -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+            <div class="row">
+                <div id="footer-widgets-1" class="col-md-6">
+                    <?php 
+                        if ( ! dynamic_sidebar( 'footer-widgets-1' ) ) :
+                            do_action( 'wpbss-footer-widgets-1' );
+                        endif; // end sidebar widget area
+                    ?>
+                </div>
+                <div id="footer-widgets-2" class="col-md-6">
+                    <?php 
+                        if ( ! dynamic_sidebar( 'footer-widgets-2' ) ) :
+                            do_action( 'wpbss-footer-widgets-2' );
+                        endif; // end sidebar widget area
+                    ?>        
+                </div>
+            </div><!-- .row -->
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
